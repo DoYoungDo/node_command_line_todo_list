3e5b8d48d726cc07ebff1f806f0b194145c57fc7
doyoung <doyoung9508@126.com>
2024-05-21 14:15:47 +0800

    fix: chinese out put error code

--- src/printer.ts
+++ src/printer.ts
@@ -1,4 +1,8 @@
 export class Printer {
+    constructor(){
+        process.stdout.setEncoding("utf-8");
+    }
+
     printTable(tabularData: any[], properties?: readonly string[],showIndex:boolean = true):void {
         console.table(tabularData, properties);
     }
